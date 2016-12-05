7f155e8ccd57dbc825e5ff5f6fbccbf910cfbfec
AlexanderOkochutin <okochutinwork@gmail.com>
2016-12-05 19:29:42 +0300

    add xml comments in queries

--- Task01.ORM/LINQqueries.cs
+++ Task01.ORM/LINQqueries.cs
@@ -8,8 +8,18 @@ using System.Threading.Tasks;
 
 namespace Task01.ORM
 {
+    /// <summary>
+    /// LINQ queries 1-6 from TASK
+    /// </summary>
     public static class LINQqueries
     {
+        /// <summary>
+        /// Querie for adding ingridient to database
+        /// </summary>
+        /// <param name="ingradientName">name of ingridient (for example "tomato")</param>
+        /// <param name="categoryName">name of ingridient category (for example "vegetables")</param>
+        /// <param name="weight">weight of ingridient (if ingredient already exist it's weight increments)</param>
+        /// <returns> bool value wich indicates success of operation </returns>
         public static bool AddIngradient(string ingradientName, string categoryName, int weight)
         {
             using (var context = new ShawarmaContext())
@@ -43,6 +53,11 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new ingredient category 
+        /// </summary>
+        /// <param name="categoryName">inredient category (for example "vegetables")</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddIngradientCategory(string categoryName)
         {
             using (var context = new ShawarmaContext())
@@ -52,7 +67,11 @@ namespace Task01.ORM
             }
         }
 
-
+        /// <summary>
+        /// querie for selling shawarma (success selling decrease weight of ingredients)
+        /// </summary>
+        /// <param name="shawarmaName">shawarma name (for example "slim-double chease")</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool SellShawarma(string shawarmaName)
         {
             using (var context = new ShawarmaContext())
@@ -79,6 +98,14 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new recipe to database
+        /// </summary>
+        /// <param name="shawarmaName"> shawarma name</param>
+        /// <param name="cookingTime"> cooking time of this shawarma </param>
+        /// <param name="ingradientNames">array of needed ingradients </param>
+        /// <param name="weights">array of weights of ingradients</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddNewRecipe(string shawarmaName, int cookingTime,
             string[] ingradientNames, int[] weights)
         {
@@ -110,6 +137,14 @@ namespace Task01.ORM
             return false;
         }
 
+        /// <summary>
+        /// querie for set new price for shawarma in some sell point
+        /// </summary>
+        /// <param name="shawarmaName">shawarma name</param>
+        /// <param name="newPrice">new price for this shawarma</param>
+        /// <param name="sellPointName">name of sell point</param>
+        /// <param name="comment">comment for new price (for example "50% for this weekend")</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool SetNewPrice(string shawarmaName, decimal newPrice, string sellPointName, string comment)
         {
             using (var context = new ShawarmaContext())
@@ -144,6 +179,11 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new category of selling point
+        /// </summary>
+        /// <param name="sellingCategoryName"> category name (for example "bistro","fast food","cafe" etc.)</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddSellingPointcategory(string sellingCategoryName)
         {
             using (var context = new ShawarmaContext())
@@ -156,6 +196,13 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new selling point
+        /// </summary>
+        /// <param name="address"> address of this selling point</param>
+        /// <param name="sellCategoryName">category of this selling point</param>
+        /// <param name="title">title of this selling point</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddNewSellPoint(string address, string sellCategoryName, string title)
         {
             using (var context = new ShawarmaContext())
@@ -179,6 +226,12 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new Seller
+        /// </summary>
+        /// <param name="name">name of seller</param>
+        /// <param name="shawarmaTitle">name of selling point</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddSeller(string name, string shawarmaTitle)
         {
             using (var context = new ShawarmaContext())
@@ -197,6 +250,13 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding time controller for exist seller
+        /// </summary>
+        /// <param name="sellerName">seller name</param>
+        /// <param name="workStart">date when seller start work</param>
+        /// <param name="workEnd"> date when seller end work</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddTimeController(string sellerName, DateTime workStart, DateTime workEnd)
         {
             using (var context = new ShawarmaContext())
@@ -219,6 +279,14 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// querie for adding new order
+        /// </summary>
+        /// <param name="shawarmaName">shawarma name</param>
+        /// <param name="date">date-time of order</param>
+        /// <param name="sellerName">seller name</param>
+        /// <param name="quantity"> quantity of shawarmas</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         public static bool AddOrder(string shawarmaName, DateTime date, string sellerName, int quantity)
         {
             using (var context = new ShawarmaContext())
@@ -241,6 +309,11 @@ namespace Task01.ORM
             }
         }
 
+        /// <summary>
+        /// SaveChanges in context
+        /// </summary>
+        /// <param name="context">database context</param>
+        /// <returns>bool value wich indicates success of operation</returns>
         private static bool Commit(DbContext context)
         {
             try
